3b1090c2e54955ab427cc6e18b1a01415d983fb6
Om Prakash Sinha <142241301+omprakashsinha9155@users.noreply.github.com>
2023-08-29 19:59:21 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello_world(request):
+	return HttpResponse("Hello world")
+
+
+def home_page(request):
+	return render(request,"task.html")
